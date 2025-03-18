3c731471c929aa1f7da28de57389697dcfd89b2c
Meerthika <meerthikasrrajendrans@gmail.com>
2025-03-18 21:41:23 +0530

    Add simple http server with get Reqest

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import(
+	"fmt"
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Welcome to GO API")
+}
+
+func main(){
+	r:=mux.NewRouter()
+	r.HandleFunc("/", homeHandler).Methods("GET")
+	fmt.Println("Server listening on Port:8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
